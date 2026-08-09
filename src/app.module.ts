03c52329989a0a247b3b291ca12c6f4aa16e6699
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-08 22:25:55 -0300

    feat: configure MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// Mongoose
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+
+        onConnectionCreate: (connection) => {
+          console.log('🍃 MongoDB connected successfully');
+
+          connection.on('error', (error) => {
+            console.error('❌ MongoDB connection error:', error);
+          });
+
+          return connection;
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
