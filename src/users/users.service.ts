56af702f1fd6bb08fe77a3079ddf68af6843dbc7
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-10 02:47:35 -0300

    feat: add user test req

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  //Função teste no /users retonrnando o nome do modelo User
+  getModelName() {
+    return this.userModel.modelName;
+  }
+}
