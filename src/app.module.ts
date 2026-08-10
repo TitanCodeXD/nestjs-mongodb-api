cfbb53e962e20f709a7602071ff5105723c9b2c2
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-10 01:49:33 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 //Módulos
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database.module'; //Database
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
