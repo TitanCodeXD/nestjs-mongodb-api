ea8e9e99129ee5e3497e610fbab80823470edc44
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-19 19:13:58 -0300

    feat: Initial Comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { DatabaseModule } from './database/database.module'; //Database
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule, PostsModule],
+  imports: [DatabaseModule, UsersModule, AuthModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
