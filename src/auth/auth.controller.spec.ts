1cc980359e529fab3ad6ece2de1fd6cf63e0f179
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-23 20:37:32 -0300

    test: auth controller tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 
+//Dto
+import { LoginDto } from './dto/login.dto';
+
 //Service
 import { AuthService } from './auth.service';
 
 //Mocks
-const mockAuthService = {};
+const mockAuthService = {
+  login: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -27,5 +32,20 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be authenticated', async () => {
+    const loginDto: LoginDto = {
+      email: 'wesley@hotmail.com',
+      password: '123',
+    };
+
+    mockAuthService.login.mockResolvedValue(loginDto);
+
+    const result = await controller.login(loginDto);
+
+    expect(result).toEqual(loginDto);
+    expect(mockAuthService.login).toHaveBeenCalledWith(loginDto);
+    expect(mockAuthService.login).toHaveBeenCalledTimes(1);
   });
 });
