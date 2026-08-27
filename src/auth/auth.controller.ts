ffdfe05afab6d2b2f26095de5fd105858c4cc3e6
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-26 21:11:37 -0300

    docs(swagger): documment all auth module endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,35 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
+//Swagger
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+
 // Public decorator
 import { Public } from './decorators/public.decorator';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
+  @ApiOperation({ summary: 'Login' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Login successful',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - invalid credentials',
+  })
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,20 @@
 import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginDto {
+  @ApiProperty({
+    example: 'wesley@hotmail.com',
+    description: 'User email',
+  })
   @IsNotEmpty()
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    example: '123456789',
+    description: 'User password',
+  })
   @IsNotEmpty()
   @IsString()
   password!: string;
