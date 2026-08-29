746c492011c68c2a7994e933d61a9bc4322e58d5
Wesley Santos <wesleysantos32892653@outlook.com>
2026-08-29 01:00:52 -0300

    feat: email.service working with EmailVerificationToken

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,7 +4,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
   @ApiProperty({
-    example: 'wesley@hotmail.com',
+    default: 'wesleysantos32892653@gmail.com',
+    example: 'wesleysantos32892653@gmail.com',
     description: 'User email',
   })
   @IsNotEmpty()
@@ -12,6 +13,7 @@ export class LoginDto {
   email!: string;
 
   @ApiProperty({
+    default: '12345',
     example: '12345',
     description: 'User password',
   })
--- src/queue/email.service.ts
+++ src/queue/email.service.ts
@@ -18,11 +18,33 @@ export class EmailService {
   }
 
   async sendVerificationEmail(to: string, token: string) {
+    const apiUrl = this.configService.get<string>('API_URL');
+
+    const verificationUrl = `${apiUrl}/auth/verify-email?token=${token}`;
     await this.transporter.sendMail({
       from: this.configService.get<string>('EMAIL_USER'),
       to,
-      subject: 'NestJS API - Test Email',
-      text: `Seu token de verificação é: ${token}`,
+      subject: `NestJS API - Verify your email ${Date.now()}`,
+      html: `<html>
+        <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
+          <div style="max-width: 600px; margin: 20px auto; padding: 20px; background: #121212; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+            <h2 style="color: #fff; text-align: center; font-family: 'Trebuchet MS', Arial, sans-serif;">Welcome to my Project!</h2>
+            <img src="https://i.imgur.com/0E9WLeU.png" alt="logo" style="max-width: 100%; height: auto; border-radius: 10px">
+            <p style="font-size: 16px; color: #666666; text-align: center;">
+              Thank you for registering with us! To complete the registration process and activate your email account, please click the button below.
+            </p>
+            <div style="text-align: center; margin-top: 20px;">
+              <a href="${verificationUrl}" style="background-color: #4CAF50; color: white; padding: 15px 25px; text-decoration: none; border-radius: 5px; font-size: 16px; font-weight: bold; display: inline-block;">
+                Verify email ✉️</i>
+              </a>
+              
+            </div>
+            <p style="font-size: 14px; color: #999999; text-align: center; margin-top: 30px;">
+              <strong>If you haven't registered with us, please, ignore this email.</strong>
+            </p>
+          </div>
+        </body>
+      </html>`,
     });
   }
 }
